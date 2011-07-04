7ffd4b042774b7947933715392b4cd5fba90739e
Karlo Smid <karlo.smid@gmail.com>
2011-07-04 21:28:27 +0200

    up to exercise 15

--- matrices.py
+++ matrices.py
@@ -6,6 +6,70 @@ __date__ ="$Jun 29, 2011 7:11:14 PM$"
 
 import copy
 
+def matrix_mult(m1, m2):
+   """
+   >>> matrix_mult([[1, 2], [3,  4]], [[5, 6], [7, 8]])
+   [[19, 22], [43, 50]]
+   >>> matrix_mult([[1, 2, 3], [4,  5, 6]], [[7, 8], [9, 1], [2, 3]])
+   [[31, 19], [85, 55]]
+   >>> matrix_mult([[7, 8], [9, 1], [2, 3]], [[1, 2, 3], [4, 5, 6]])
+   [[39, 54, 69], [13, 23, 33], [14, 19, 24]]
+   """
+   rez = []
+   for row in range(len(m1)):
+        rez += [[0]*len(m2[0])]
+   for i in range (len(m1)):
+       for j in range (len(m2[0])):
+           rez[i][j] = row_times_column(m1, i, m2, j)
+   print rez
+def row_times_column(m1, row, m2, column):
+    """
+    >>> print row_times_column([[1,2],[3,4]],1,[[1,2,3],[1,2,3],[1,2,3]],0)
+    -2
+    >>> print row_times_column(1,2,[[1,2],[3,4]],4)
+    -1
+    >>> print row_times_column([[1,2],[2,4]],2,3,4)
+    -1
+    >>> print row_times_column([[1, 2], [3, 4]], 0, [[5, 6], [7, 8]], 0)
+    19
+    >>> print row_times_column([[1, 2], [3, 4]], 0, [[5, 6], [7, 8]], 1)
+    22
+    >>> print row_times_column([[1, 2], [3, 4]], 1, [[5, 6], [7, 8]], 0)
+    43
+    >>> row_times_column([[1, 2], [3, 4]], 1, [[5, 6], [7, 8]], 1)
+    50
+    """    
+    if not isinstance(m1,list) or not isinstance(m2,list):
+        return -1
+    elif len(m1[0]) != len(m2):
+        return -2
+    rez = 0    
+    for i in range(len(m1[0])):               
+        rez = rez + m1[row][i] * m2[i][column]
+    return rez
+
+def scalar_mult(s, m):
+    """
+    >>> scalar_mult(1,2)
+    -1
+    >>> scalar_mult([1],[[1,2]])
+    -1
+    >>> a = [[1, 2], [3, 4]]
+    >>> scalar_mult(3, a)
+    [[3, 6], [9, 12]]
+    >>> b = [[3, 5, 7], [1, 1, 1], [0, 2, 0], [2, 2, 3]]
+    >>> scalar_mult(10, b)
+    [[30, 50, 70], [10, 10, 10], [0, 20, 0], [20, 20, 30]]
+    >>> b
+    [[3, 5, 7], [1, 1, 1], [0, 2, 0], [2, 2, 3]]
+    """
+    rez = copy.deepcopy(m)
+    if not isinstance(m,list) or not isinstance(s,int):
+        return -1
+    for (o,outer) in enumerate(m):
+        for (i,inner) in enumerate(outer):
+            rez[o][i] = s * rez[o][i]
+    print rez
 def add_matrices(m1, m2):
     """
     >>> add_matrices(1,[0])
