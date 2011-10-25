0471c6b90fa64d680a1647a66215d1b3b299130a
Karlo Smid <karlo.smid@gmail.com>
2011-10-25 20:20:49 +0200

    recursive_count done

--- seqtools.py
+++ seqtools.py
@@ -228,6 +228,24 @@ def recursive_min(nested_num_list):
         if type(item) == type([]):
             return recursive_min(item)
     return sort_sequence(nested_num_list)[0]
+def recursive_count(target, nested_num_list):
+    """
+      >>> recursive_count(2, [2, 9, [2, 1, 13, 2], 8, [2, 6]])
+      4
+      >>> recursive_count(7, [[9, [7, 1, 13, 2], 8], [7, 6]])
+      2
+      >>> recursive_count(15, [[9, [7, 1, 13, 2], 8], [2, 6]])
+      0
+      >>> recursive_count(5, [[5, [5, [1, 5], 5], 5], [5, 6]])
+      6
+    """
+    count = 0
+    for item in nested_num_list:
+        if type(item) == type([]):
+            count = count + recursive_count(target,item)
+        if item == target:
+            count = count + 1
+    return count
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
