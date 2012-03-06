b8f11fdd6923f9ffa0381ffef44c2e5ea9c948e4
Karlo Smid <karlo.smid@gmail.com>
2012-03-06 20:56:45 +0100

    first communication with diigo API

--- selectFromLinkDiigoTag.py
+++ selectFromLinkDiigoTag.py
@@ -1,10 +1,10 @@
 import getopt, sys
 
 def usage():
-    print 'usage: selectFromLinkDiigoTag.py -i fileWithLinks'
+    print 'usage: selectFromLinkDiigoTag.py -i fileWithLinks -d'
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:")
+        opts, args = getopt.getopt(sys.argv[1:], "i:d")
     except getopt.GetoptError, err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
@@ -12,7 +12,21 @@ def main():
     inputFileName = None
     for o, a in opts:
         if o == "-i":
-            inputFileName = a 
+            inputFileName = a
+            try: 
+                fileWithLinks = open(inputFileName,'r')
+            except Exception, e:
+                print str(e)
+                usage()
+                sys.exit(2)
+            listWithDiigoTags = parseFileWithLinks(fileWithLinks)
+            fileWithLinks.close()
+            file = open(inputFileName[:-4]+'WithTags.txt','w')
+            file.write('\n'.join(listWithDiigoTags))
+            file.close()
+        if o == "-d":
+            getDiigoLinks()
+            sys.exit(2)
         else:
             assert False, "unhandled option"
     try: 
@@ -26,6 +40,19 @@ def main():
     file = open(inputFileName[:-4]+'WithTags.txt','w')
     file.write('\n'.join(listWithDiigoTags))
     file.close()
+def sendToDiigo(diigoFileWithLinksAndTags):
+    diigoApiKey = 'eb850240326fddfd'
+def getDiigoLinks():
+    import urllib2
+    import base64
+    username = 'karlosmid'
+    password = 'dirl63ruG*'
+    base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
+    request =\
+    urllib2.Request('https://secure.diigo.com/api/v2/bookmarks?key=eb850240326fddfd&user=karlosmid&count=10')
+    request.add_header("Authorization", "Basic %s" % base64string)
+    stream = urllib2.urlopen(request)
+    print stream.read(1000)
 def parseFileWithLinks(fileWithLinks):
     LINK_NAME = 1
     LINK = 0
