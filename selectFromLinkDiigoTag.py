3a5dd1192612423aff988a6ded75b14aa4a04b21
Karlo Smid <karlo.smid@gmail.com>
2012-03-07 22:00:35 +0100

    and diigo saving prototype is working

--- selectFromLinkDiigoTag.py
+++ selectFromLinkDiigoTag.py
@@ -1,10 +1,10 @@
 import getopt, sys
 
 def usage():
-    print 'usage: selectFromLinkDiigoTag.py -i fileWithLinks -d'
+    print 'usage: selectFromLinkDiigoTag.py -i fileWithLinks -r numberOfDiigoLinks -s diigoFileWithTags'
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:d")
+        opts, args = getopt.getopt(sys.argv[1:], "irs:")
     except getopt.GetoptError, err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
@@ -24,8 +24,11 @@ def main():
             file = open(inputFileName[:-4]+'WithTags.txt','w')
             file.write('\n'.join(listWithDiigoTags))
             file.close()
-        if o == "-d":
-            getDiigoLinks()
+        elif o == "-r":
+            getDiigoLinks(a)
+            sys.exit(2)
+        elif o == "-s":
+            sendToDiigo(a)
             sys.exit(2)
         else:
             assert False, "unhandled option"
@@ -41,20 +44,42 @@ def main():
     file.write('\n'.join(listWithDiigoTags))
     file.close()
 def sendToDiigo(diigoFileWithLinksAndTags):
-    diigoApiKey = 'eb850240326fddfd'
-def getDiigoLinks():
+    import urllib
     import urllib2
-    import base64
+    url = 'https://secure.diigo.com/api/v2/bookmarks'
+    diigoApiKey = 'eb850240326fddfd'
     username = 'karlosmid'
+    values = {'key':diigoApiKey,
+              'url':'http://zagorskisoftwaretester.blogspot.com/',
+              'title':'Moj blog',
+              'tags':'testing',
+              'shared':'yes'}
+    data = urllib.urlencode(values)
+    request = urllib2.Request(url,data)
+    authorisedRequest = authoriseRequest(request,username)
+    response = urllib2.urlopen(authorisedRequest)
+    response_page = response.read()
+    print response_page
+def getDiigoLinks(numberOfDiigoLinks):
+    import urllib2
+    diigoApiKey = 'eb850240326fddfd'
+    username = 'karlosmid'
+    request =\
+    urllib2.Request('https://secure.diigo.com/api/v2/bookmarks?key='+diigoApiKey+'&user='+username+'&count='+numberOfDiigoLinks)
+    authorisedRequest = authoriseRequest(request,username) 
+    try: 
+        stream = urllib2.urlopen(authorisedRequest)
+    except urllib2.URLError,e:
+        print e
+    print stream.read()
+def authoriseRequest(request,username):
+    import base64
     fileWithPassword = open('diigo.txt','r')
     password = fileWithPassword.readline()
     fileWithPassword.close()
     base64string = base64.encodestring('%s:%s' % (username, password[:-1]))[:-1]
-    request =\
-    urllib2.Request('https://secure.diigo.com/api/v2/bookmarks?key=eb850240326fddfd&user=karlosmid&count=10')
     request.add_header("Authorization", "Basic %s" % base64string)
-    stream = urllib2.urlopen(request)
-    print stream.read()
+    return request
 def parseFileWithLinks(fileWithLinks):
     LINK_NAME = 1
     LINK = 0
